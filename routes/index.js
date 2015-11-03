fa3c1006ebc46dd1f8908ff7736279175a02220d
Sarabpreet Singh <Sarabpreetsingh23@gmail.com>
2015-11-03 22:12:05 +0530

    more code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'API Testing Tool' });
 });
 
 module.exports = router;
